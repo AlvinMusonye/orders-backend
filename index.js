079c07fc7cb5850c80f594a2994309251f6a3d69
Manasseh Gitau <kimmanasseh14@gmail.com>
2025-03-27 12:55:03 +0300

    feat: add transcation controller to node app

--- index.js
+++ index.js
@@ -14,68 +14,6 @@ const PORT = process.env.PORT;
 connectDB();
 
 
-
-
-
-export const getTokenRequest = async (req, res) => {
-    try {
-        // Base64 encode the key and secret
-        const credentials = Buffer.from(`${process.env.BUNI_USERNAME}:${process.env.BUNI_PASSWORD}`).toString('base64');
-        const tokenurl = process.env.BUNI_TOKEN_REQUEST_URL;
-        const token = await fetch(tokenurl,{
-            method: "POST",
-            headers: {
-                "Authorization": `Basic ${credentials} `,
-            }
-        })
-        const tokenData = await token.json();
-        // console.log(tokenData.access_token);
-        localStorage.setItem("AuthToken",tokenData.access_token);
-    //   console.log(token)
-    } catch (err) {
-        // res.status(500).json({ error: err.message });
-    }
-};
-getTokenRequest()
-
-// STK Push request details
-const phoneNumber = '25493892167';  // Replace with actual phone number
-const amount = '1';                  // Amount to transfer
-const invoiceNumber = 'Alvinjrfoodplace';  // Replace with actual invoice number
-const callbackUrl = 'https://uat.buni.kcbgroup.com/mm/api/request/1.0.0/stkpush';  // Your callback URL
-const sharedShortCode = true;         // Set to true for KCB's short code
-
-
-
-export const STKpush = async(req, res) => {
-    try {
-        const token = localStorage.getItem("AuthToken");
-        console.log(token);
-        const response = await fetch(process.env.STK_PUSH_URL, {
-            method: "POST",
-            headers: {
-                "Authorization": `Bearer ${token} `,
-            },
-            body: JSON.stringify({
-                phoneNumber: phoneNumber,
-                amount: amount,
-                invoiceNumber: invoiceNumber,
-                sharedShortCode: sharedShortCode,
-                orgShortCode: '',             // Optional
-                orgPassKey: '',               // Optional
-                callbackUrl: callbackUrl,
-                transactionDescription: 'Payment for goods'  // Optional description
-              })
-        });
-        const responseData = await response.json();
-    } catch (err) {
-        // res.status(500).json({ error: err.message});
-    }
-};
-
-STKpush()
-
-
 app.get("/", cors, (req, res) => res.send("Task API is running"));
 
 app.listen(PORT, () => {
